0cf5445b5cbb0ea03eea05226c78eae4e5839192
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2018-10-31 22:26:47 -0300

    Arrumando a estrutura para que a aplicacao possa encontrar o controller

--- src/main/java/br/com/mantendoofoco/MantendoOFocoApplication.java
+++ src/main/java/br/com/mantendoofoco/MantendoOFocoApplication.java
@@ -1,7 +1,8 @@
-package br.com.mantendoofoco.mantendoofoco;
+package br.com.mantendoofoco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class MantendoOFocoApplication {
