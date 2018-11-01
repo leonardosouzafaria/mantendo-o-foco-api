ec5bb5154ee506f1e77ef05b76c3887533e09f10
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2018-10-31 22:26:20 -0300

    Feito classe de acesso ao configuracao do banco de dados

---
+++ src/main/java/br/com/mantendoofoco/DataConfiguration.java
@@ -0,0 +1,36 @@
+package br.com.mantendoofoco;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfiguration {
+
+	@Bean
+	public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://us-cdbr-iron-east-01.cleardb.net/heroku_0bfd010abe48434");
+        dataSource.setUsername("b3d900973edf84");
+        dataSource.setPassword("1e258d22");
+        return dataSource;
+    }
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
+
+}
